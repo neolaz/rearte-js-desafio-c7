c9ef724049483b04f4b2a29eaec9086311cf071b
Neolaz <reartemarcos@gmail.com>
2022-09-21 19:59:51 -0300

    Entrega completa

--- js/main.js
+++ js/main.js
@@ -9,10 +9,19 @@ const calcularTotal = (listaDocumentoDetalle) => {
     let total = 0;
 
     listaDocumentoDetalle.forEach( (det) => {
-        impuesto = 1 + det.impuesto / 100;
-        total += det.precio * det.cantidad * impuesto;
+        impuesto = 1 + det.producto.impuesto / 100;
+        total += det.producto.precio * det.cantidad * impuesto;
     });
     return total;
+}
+
+class Producto{
+    constructor (id, nombre, precio, impuesto){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.impuesto = impuesto;
+    }
 }
 
 class Documento{
@@ -24,49 +33,44 @@ class Documento{
     }
 
     mostrarDocumento(){
-        alert(`${this.tipo} número ${this.numero} por un total de $${this.total}. `)
+        let documentoStr;
+
+        documentoStr = `${this.tipo} número ${this.numero} por un total de $${this.total}. `;
+        return documentoStr;
     }
 
     mostrarMensajeDocumentoCreado(){
         let mensajeDocumento;
 
         mensajeDocumento = `Se creó correctamente la ${this.tipo} número ${this.numero} por un total de $${this.total}. La misma contiene los siguientes productos: `;
-        this.listaDocumentoDetalle.forEach( (det) => {  mensajeDocumento += `\n${det.cantidad} x ${det.producto} con un precio de $${det.precio} c/u y un ${det.impuesto}% de impuestos.`;  });
+        this.listaDocumentoDetalle.forEach( (det) => {  mensajeDocumento += `\n${det.cantidad} x ${det.producto.nombre} con un precio de $${det.producto.precio} c/u y un ${det.producto.impuesto}% de impuestos.`;  });
         alert(mensajeDocumento);
     }
 }
 
 class DocumentoDetalle{
-    constructor (producto, cantidad, precio, impuesto){
+    constructor (producto, cantidad){
         this.producto = producto;
         this.cantidad = cantidad;
-        this.precio = precio;
-        this.impuesto = impuesto;
     }
 }
 
-// Bucle principal
-while (iterador){
-    opcionMenu = gestionarMenu();
-    if (opcionMenu == 1) crearDocumento();
-    if (opcionMenu == 2) mostrarListaDocumentos();
-    if (opcionMenu == 3) iterador = false;
-}
+let productos = [new Producto(1, 'Microfono', 25000, 21), new Producto(2, 'Monitor', 80000, 0), new Producto(3, 'Mouse', 20000, 21), new Producto(4, 'Parlantes', 50000, 27), new Producto(5, 'Teclado', 30000, 10.5)];
 
-function gestionarMenu(){
+const gestionarMenu = () => {
     inputOpcionMenu = prompt('Bienvenido a Coder Facturación! Ingrese:\n1 - Para crear un Documento.\n2 - Para mostrar los Documentos creados.\n3 - Para salir.');
     opcionMenu = validarInputMenu(inputOpcionMenu);
     return opcionMenu;
 }
 
-function validarInputMenu(inputOpcionMenu){
+const validarInputMenu = (inputOpcionMenu) => {
     while(!(inputOpcionMenu == '1' || inputOpcionMenu == '2' || inputOpcionMenu == '3')){
         inputOpcionMenu = prompt('Opción inválida. Ingrese:\n1 - Para crear un Documento.\n2 - Para mostrar los Documentos creados.\n3 - Para salir.');
     }
     return inputOpcionMenu;
 }
 
-function crearDocumento(){    
+const crearDocumento = () => {    
     let tipoDocumento;
     let numeroDocumento;
     let listaDocumentoDetalle;
@@ -80,7 +84,7 @@ function crearDocumento(){
     documento.mostrarMensajeDocumentoCreado();
 }
 
-function seleccionarTipoDocumento(){
+const seleccionarTipoDocumento = () => {
     let inputTipoDocumento;
     let tipoDocumento;
 
@@ -89,7 +93,7 @@ function seleccionarTipoDocumento(){
     return tipoDocumento;
 }
 
-function validarInputTipoDocumento(inputTipoDocumento){
+const validarInputTipoDocumento = (inputTipoDocumento) => {
     let tipoDocumento;
 
     while(!(inputTipoDocumento == '1' || inputTipoDocumento == '2' || inputTipoDocumento == '3')){
@@ -101,111 +105,7 @@ function validarInputTipoDocumento(inputTipoDocumento){
     return tipoDocumento;
 }
 
-function cargarDetalle(){
-    let producto;
-    let precio;
-    let cantidad;
-    let impuesto;
-    let listaDocumentoDetalle = [];
-    let documentoDetalle;
-    let iteradorDetalle = true;
-    let inputOpcionDetalle;
-    let opcionDetalle;
-
-    while (iteradorDetalle) {
-        producto = ingresarProducto();
-        precio = ingresarPrecio();
-        cantidad = ingresarCantidad();
-        impuesto = seleccionarImpuesto();
-        documentoDetalle = new DocumentoDetalle(producto, cantidad, precio, impuesto);
-        listaDocumentoDetalle.push(documentoDetalle);
-
-        inputOpcionDetalle = prompt('Desea cargar otra línea de detalle al documento? Ingrese "S" para sí o "N" para no.');
-        opcionDetalle = validarInputOpcionDetalle(inputOpcionDetalle);
-
-        if(opcionDetalle.toUpperCase() == 'N') iteradorDetalle = false;
-    }
-    return listaDocumentoDetalle;
-}
-
-function validarInputOpcionDetalle(inputOpcionDetalle){
-    while(!(inputOpcionDetalle.toUpperCase() == 'S' || inputOpcionDetalle.toUpperCase() == 'N')){
-        inputOpcionDetalle = prompt('Opción inválida. Ingrese "S" para sí o "N" para no.');
-    }  
-    return inputOpcionDetalle;
-}
-
-function ingresarProducto(){
-    let inputProducto;
-    let producto;
-
-    inputProducto = prompt('Ingrese el producto que desea facturar.');
-    producto = validarInputProducto(inputProducto);
-    return producto;
-}
-
-function validarInputProducto(inputProducto){
-    while(inputProducto == ''){
-        inputProducto = prompt('Error. Ingrese el producto que desea facturar.');
-    }  
-    return inputProducto;
-}
-
-function ingresarPrecio(){
-    let inputPrecio;
-    let precio;
-
-    inputPrecio = prompt('Ingrese el precio del producto.');
-    precio = validarInputPrecio(inputPrecio);
-    return precio;
-}
-
-function validarInputPrecio(inputPrecio){
-    while(inputPrecio < 1){
-        inputPrecio = prompt('El precio debe ser mayor que 0, ingréselo nuevamente.');
-    }  
-    return inputPrecio;
-}
-
-function ingresarCantidad(){
-    let inputCantidad;
-    let cantidad;
-
-    inputCantidad = prompt('Ingrese la cantidad del producto.');
-    cantidad = validarInputCantidad(inputCantidad);
-    return cantidad;
-}
-
-function validarInputCantidad(inputCantidad){
-    while(inputCantidad < 1){
-        inputCantidad = prompt('La cantidad debe ser mayor que 0, ingrésela nuevamente.');
-    }  
-    return inputCantidad;
-}
-
-function seleccionarImpuesto(){
-    let inputImpuesto;
-    let impuesto;
-
-    inputImpuesto = prompt('Seleccione el impuesto correspondiente para el producto! Ingrese:\n1 - 0%\n2 - 10.5%.\n3 - 21%.\n4 - 27%.');
-    impuesto = validarInputImpuesto(inputImpuesto);
-    return impuesto;
-}
-
-function validarInputImpuesto(inputImpuesto){
-    let impuesto;
-
-    while(!(inputImpuesto == '1' || inputImpuesto == '2' || inputImpuesto == '3' || inputImpuesto == '4')){
-        inputImpuesto = prompt('Opción inválida. Ingrese:\n1 - 0%\n2 - 10.5%.\n3 - 21%.\n4 - 27%.');
-    }  
-    if (inputImpuesto == '1') impuesto = 0.00
-    if (inputImpuesto == '2') impuesto = 10.50;
-    if (inputImpuesto == '3') impuesto = 21.00;
-    if (inputImpuesto == '4') impuesto = 27.00;
-    return impuesto;
-}
-
-function asignarNumero(tipoDocumento){
+const asignarNumero = (tipoDocumento) => {
     let numeroDocumento;
 
     if (tipoDocumento == 'Factura') {
@@ -223,22 +123,96 @@ function asignarNumero(tipoDocumento){
     return numeroDocumento;
 }
 
-function mostrarListaDocumentos(){
+const cargarDetalle = () => {
+    let indexProducto;
+    let cantidad;
+    let listaDocumentoDetalle = [];
+    let documentoDetalle;
+    let iteradorDetalle = true;
+    let inputOpcionDetalle;
+    let opcionDetalle;
+
+    while (iteradorDetalle) {
+        indexProducto = seleccionarProducto();
+        indexProducto--;
+        cantidad = ingresarCantidad();
+        documentoDetalle = new DocumentoDetalle(productos[indexProducto], cantidad);
+        listaDocumentoDetalle.push(documentoDetalle);
+
+        inputOpcionDetalle = prompt('Desea cargar otra línea de detalle al documento? Ingrese "S" para sí o "N" para no.');
+        opcionDetalle = validarInputOpcionDetalle(inputOpcionDetalle);
+
+        if(opcionDetalle.toUpperCase() == 'N') iteradorDetalle = false;
+    }
+    return listaDocumentoDetalle;
+}
+
+const validarInputOpcionDetalle = (inputOpcionDetalle) => {
+    while(!(inputOpcionDetalle.toUpperCase() == 'S' || inputOpcionDetalle.toUpperCase() == 'N')){
+        inputOpcionDetalle = prompt('Opción inválida. Ingrese "S" para sí o "N" para no.');
+    }  
+    return inputOpcionDetalle;
+}
+
+const seleccionarProducto = () => {
+    let mensajeInputProducto;
+    let inputProducto;
+    let producto;
+
+    mensajeInputProducto = 'Seleccione el Producto que desea facturar! Ingrese: ';
+    productos.forEach( (p) => {  mensajeInputProducto += `\n${p.id} - ${p.nombre} por $${p.precio} con un ${p.impuesto}% de impuestos.`;  });
+
+    inputProducto = prompt(mensajeInputProducto);
+    producto = validarInputProducto(inputProducto);
+    return producto;
+}
+
+const validarInputProducto = (inputProducto) => {
+    let mensajeInputProducto;
+
+    mensajeInputProducto = 'Opción inválida! Ingrese: ';
+    productos.forEach( (p) => {  mensajeInputProducto += `\n${p.id} - ${p.nombre} por $${p.precio} con un ${p.impuesto}% de impuestos.`;  });
+    while(inputProducto < 1 || inputProducto > 5){
+        inputProducto = prompt(mensajeInputProducto);
+    }  
+    return inputProducto;
+}
+
+const ingresarCantidad = () => {
+    let inputCantidad;
+    let cantidad;
+
+    inputCantidad = prompt('Ingrese la cantidad del producto.');
+    cantidad = validarInputCantidad(inputCantidad);
+    return cantidad;
+}
+
+const validarInputCantidad  = (inputCantidad) => {
+    while(inputCantidad < 1){
+        inputCantidad = prompt('La cantidad debe ser mayor que 0, ingrésela nuevamente.');
+    }  
+    return inputCantidad;
+}
+
+const mostrarListaDocumentos = () => {
+    let listaDocumentosStr = '';
+
     if(listaDocumentos.length == 0){
         alert("Todavía no hay documentos creados.");
         return;
     }
-    listaDocumentos.forEach( (doc) => { doc.mostrarDocumento() });
+    console.log(listaDocumentosStr);
+    listaDocumentosStr += 'Se crearon los siguientes documentos: ';
+    console.log(listaDocumentosStr);
+    listaDocumentos.forEach( (doc) => { listaDocumentosStr +=  '\n'+ doc.mostrarDocumento() });
+    console.log(listaDocumentosStr);
+    alert(listaDocumentosStr);
 }
 
-// function calcularTotal(listaDocumentoDetalle){
-//     let impuesto;
-//     let total = 0;
-
-//     listaDocumentoDetalle.forEach( (det) => {
-//         impuesto = 1 + det.impuesto / 100;
-//         total += det.precio * det.cantidad * impuesto;
-//     });
-//     return total;
-// }
-
+// Bucle principal
+while (iterador){
+    opcionMenu = gestionarMenu();
+    if (opcionMenu == 1) crearDocumento();
+    if (opcionMenu == 2) mostrarListaDocumentos();
+    if (opcionMenu == 3) iterador = false;
+}
